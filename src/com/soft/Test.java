0003d626205b14d0119dcfbad2f311f59f12d176
VitaliiKrasov <krasovvitalii@gmail.com>
2018-11-10 19:10:12 +0200

    added few methods

--- src/com/soft/Test.java
+++ src/com/soft/Test.java
@@ -1,4 +1,28 @@
 package com.soft;
 
-public class Test {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Test {
+
+    @org.junit.jupiter.api.Test
+    void add() {
+        assertEquals(9, Calculator.add(4, 5));
+        assertEquals(-8, Calculator.add(-10,2));
+    }
+
+    @org.junit.jupiter.api.Test
+    void subtract() {
+        assertEquals(-5, Calculator.subtract(10, 15));
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiply() {
+        assertEquals(24, Calculator.multiply(8, 3));
+    }
+
+    @org.junit.jupiter.api.Test
+    void divide() {
+        assertEquals(2, Calculator.divide(50, 24));
+    }
+
 }
